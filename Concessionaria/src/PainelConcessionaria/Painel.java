ce6e063490cda359f12844434afb0c0ba96877d4
Vanderley <vanderley196338@gmail.com>
2024-05-24 11:17:24 -0300

    Projeto Sexta

--- Concessionaria/src/PainelConcessionaria/Painel.java
+++ Concessionaria/src/PainelConcessionaria/Painel.java
@@ -1,4 +1,11 @@
+package PainelConcessionaria;
+
+import java.util.LinkedList;
+
 public class Painel extends javax.swing.JFrame {
+    
+    LinkedList fila = new LinkedList();
+    
     public Painel() {
         initComponents();
     }
@@ -7,12 +14,11 @@ public class Painel extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        ListaGeral = new javax.swing.ButtonGroup();
         nomeConcessionaria = new javax.swing.JLabel();
-        BotaoCarro = new javax.swing.JButton();
-        BotaoMoto = new javax.swing.JButton();
-        BotaoCaminhao = new javax.swing.JButton();
+        BotaoPegar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
+        Area = new javax.swing.JTextArea();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
@@ -23,95 +29,44 @@ public class Painel extends javax.swing.JFrame {
         Valor = new javax.swing.JTextField();
         jLabel7 = new javax.swing.JLabel();
         Ano = new javax.swing.JTextField();
-        BotaoFinalizarCarro = new javax.swing.JButton();
-        BotaoFinalizarMoto = new javax.swing.JButton();
-        BotaoFinalizarCaminhao = new javax.swing.JButton();
+        BotaoCriar = new javax.swing.JButton();
         jLabel8 = new javax.swing.JLabel();
         BotaoRemover = new javax.swing.JButton();
+        ListaCarro = new javax.swing.JRadioButton();
+        ListaMotos = new javax.swing.JRadioButton();
+        ListaCaminhao = new javax.swing.JRadioButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         nomeConcessionaria.setFont(new java.awt.Font("Segoe UI", 0, 36)); // NOI18N
         nomeConcessionaria.setText("CONCESSIONARIA SENA");
 
-        BotaoCarro.setText("Carros");
-        BotaoCarro.addActionListener(new java.awt.event.ActionListener() {
+        BotaoPegar.setText("Pegar");
+        BotaoPegar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                BotaoCarroActionPerformed(evt);
+                BotaoPegarActionPerformed(evt);
             }
         });
 
-        BotaoMoto.setText("Motos");
-        BotaoMoto.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                BotaoMotoActionPerformed(evt);
-            }
-        });
-
-        BotaoCaminhao.setText("Caminhões");
-        BotaoCaminhao.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                BotaoCaminhaoActionPerformed(evt);
-            }
-        });
-
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
+        Area.setColumns(20);
+        Area.setRows(5);
+        jScrollPane1.setViewportView(Area);
 
         jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jLabel3.setText("ADICIONAR AUTOMOVEIS");
 
         jLabel4.setText("Marca:");
 
-        Marca.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                MarcaActionPerformed(evt);
-            }
-        });
-
         jLabel5.setText("Modelo:");
-
-        Modelo.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                ModeloActionPerformed(evt);
-            }
-        });
 
         jLabel6.setText("Valor:");
 
-        Valor.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                ValorActionPerformed(evt);
-            }
-        });
-
         jLabel7.setText("Ano:");
 
-        Ano.addActionListener(new java.awt.event.ActionListener() {
+        BotaoCriar.setText("Criar");
+        BotaoCriar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                AnoActionPerformed(evt);
-            }
-        });
-
-        BotaoFinalizarCarro.setText("Carros");
-        BotaoFinalizarCarro.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                BotaoFinalizarCarroActionPerformed(evt);
-            }
-        });
-
-        BotaoFinalizarMoto.setText("Motos");
-        BotaoFinalizarMoto.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                BotaoFinalizarMotoActionPerformed(evt);
-            }
-        });
-
-        BotaoFinalizarCaminhao.setText("Caminhões");
-        BotaoFinalizarCaminhao.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                BotaoFinalizarCaminhaoActionPerformed(evt);
+                BotaoCriar(evt);
             }
         });
 
@@ -125,6 +80,20 @@ public class Painel extends javax.swing.JFrame {
             }
         });
 
+        ListaGeral.add(ListaCarro);
+        ListaCarro.setSelected(true);
+        ListaCarro.setText("Carro");
+        ListaCarro.setActionCommand("Carros");
+
+        ListaGeral.add(ListaMotos);
+        ListaMotos.setText("Motos");
+        ListaMotos.setToolTipText("");
+        ListaMotos.setActionCommand("BMotos");
+
+        ListaGeral.add(ListaCaminhao);
+        ListaCaminhao.setText("Caminhão");
+        ListaCaminhao.setActionCommand("Caminhao");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -132,14 +101,10 @@ public class Painel extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(50, 50, 50)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(BotaoCarro)
-                        .addGap(18, 18, Short.MAX_VALUE)
-                        .addComponent(BotaoMoto)
-                        .addGap(18, 18, Short.MAX_VALUE)
-                        .addComponent(BotaoCaminhao, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGap(18, 18, Short.MAX_VALUE)
-                        .addComponent(BotaoRemover, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addComponent(BotaoPegar, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(BotaoRemover, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(nomeConcessionaria, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.LEADING))
                 .addGap(70, 70, 70)
@@ -150,9 +115,7 @@ public class Painel extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                             .addComponent(jLabel8, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(BotaoFinalizarCarro, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(BotaoFinalizarMoto, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(BotaoFinalizarCaminhao, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(BotaoCriar, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(Ano, javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(Valor, javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(Marca, javax.swing.GroupLayout.Alignment.LEADING)
@@ -160,18 +123,24 @@ public class Painel extends javax.swing.JFrame {
                             .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(Modelo, javax.swing.GroupLayout.Alignment.LEADING))
                         .addGap(92, 92, 92)
-                        .addComponent(jLabel2)))
+                        .addComponent(jLabel2))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(ListaCarro)
+                        .addGap(8, 8, 8)
+                        .addComponent(ListaMotos)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(ListaCaminhao)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(20, 20, 20)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(nomeConcessionaria)
-                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(nomeConcessionaria))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel4)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -188,74 +157,57 @@ public class Painel extends javax.swing.JFrame {
                         .addComponent(jLabel7)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(Ano, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(60, 60, 60)
+                        .addGap(51, 51, 51)
                         .addComponent(jLabel8)
-                        .addGap(18, 18, 18)
-                        .addComponent(BotaoFinalizarCarro)
-                        .addGap(18, 18, 18)
-                        .addComponent(BotaoFinalizarMoto)
-                        .addGap(18, 18, 18)
-                        .addComponent(BotaoFinalizarCaminhao))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(ListaCarro)
+                            .addComponent(ListaMotos)
+                            .addComponent(ListaCaminhao))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(BotaoCriar))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(137, 137, 137)
                         .addComponent(jLabel2))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(21, 21, 21)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(BotaoCarro)
-                            .addComponent(BotaoMoto)
-                            .addComponent(BotaoCaminhao)
+                            .addComponent(BotaoPegar)
                             .addComponent(BotaoRemover))
                         .addGap(18, 18, 18)
-                        .addComponent(jScrollPane1)))
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 371, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(20, 20, 20))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void BotaoCarroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotaoCarroActionPerformed
+    private void BotaoPegarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotaoPegarActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_BotaoCarroActionPerformed
+        if (fila.getFirst() != null) {
+            Area.setText( fila.getFirst().toString() );
+            fila.addLast(fila.getFirst());
+            fila.removeFirst();
+        }
+    }//GEN-LAST:event_BotaoPegarActionPerformed
 
-    private void BotaoMotoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotaoMotoActionPerformed
+    private void BotaoCriar(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotaoCriar
         // TODO add your handling code here:
-    }//GEN-LAST:event_BotaoMotoActionPerformed
-
-    private void BotaoCaminhaoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotaoCaminhaoActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_BotaoCaminhaoActionPerformed
-
-    private void MarcaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MarcaActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_MarcaActionPerformed
-
-    private void ModeloActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ModeloActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_ModeloActionPerformed
-
-    private void ValorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ValorActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_ValorActionPerformed
-
-    private void AnoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AnoActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_AnoActionPerformed
-
-    private void BotaoFinalizarCarroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotaoFinalizarCarroActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_BotaoFinalizarCarroActionPerformed
-
-    private void BotaoFinalizarMotoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotaoFinalizarMotoActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_BotaoFinalizarMotoActionPerformed
-
-    private void BotaoFinalizarCaminhaoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotaoFinalizarCaminhaoActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_BotaoFinalizarCaminhaoActionPerformed
+        String Mar = Marca.getText();
+        String Mod = Modelo.getText();
+        float Price = Float.parseFloat(Valor.getText());
+        int Year = Integer.parseInt(Ano.getText());
+        String Tipo = ListaGeral.getSelection().getActionCommand();
+        
+        fila.addLast(new Concessionaria(Mar,Mod,Tipo,Year,Price));
+    }//GEN-LAST:event_BotaoCriar
 
     private void BotaoRemoverActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotaoRemoverActionPerformed
         // TODO add your handling code here:
+        if (fila.getFirst() != null) {
+            Area.setText("");
+            fila.removeLast();
+        }
     }//GEN-LAST:event_BotaoRemoverActionPerformed
 
     /**
@@ -295,13 +247,14 @@ public class Painel extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField Ano;
-    private javax.swing.JButton BotaoCaminhao;
-    private javax.swing.JButton BotaoCarro;
-    private javax.swing.JButton BotaoFinalizarCaminhao;
-    private javax.swing.JButton BotaoFinalizarCarro;
-    private javax.swing.JButton BotaoFinalizarMoto;
-    private javax.swing.JButton BotaoMoto;
+    private javax.swing.JTextArea Area;
+    private javax.swing.JButton BotaoCriar;
+    private javax.swing.JButton BotaoPegar;
     private javax.swing.JButton BotaoRemover;
+    private javax.swing.JRadioButton ListaCaminhao;
+    private javax.swing.JRadioButton ListaCarro;
+    private javax.swing.ButtonGroup ListaGeral;
+    private javax.swing.JRadioButton ListaMotos;
     private javax.swing.JTextField Marca;
     private javax.swing.JTextField Modelo;
     private javax.swing.JTextField Valor;
@@ -313,7 +266,6 @@ public class Painel extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea jTextArea1;
     private javax.swing.JLabel nomeConcessionaria;
     // End of variables declaration//GEN-END:variables
 }
